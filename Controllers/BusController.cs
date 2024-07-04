a459b6c6e62b297db7898057a45737f27b00679e
agrmet <agron.metaj.3321@student.uu.se>
2024-07-04 13:17:07 +0200

    Basic CRUD API for Buses

--- Controllers/BusController.cs
+++ Controllers/BusController.cs
@@ -8,10 +8,59 @@ namespace BusTicketSystem.Controllers;
 [Route("[controller]")]
 public class BusController(BusService service) : ControllerBase
 {
-
     private readonly BusService _service = service;
-
+    // GET all action
     [HttpGet]
     public ActionResult<List<Bus>> GetAll() =>
     _service.GetAll().ToList();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Bus> Get(int id)
+    {
+        var bus = _service.Get(id);
+
+        if (bus is null)
+        {
+            return NotFound();
+        }
+        return bus;
+    }
+    // POST action
+    [HttpPost]
+    public IActionResult Create(Bus bus)
+    {
+        _service.Create(bus);
+        return CreatedAtAction(nameof(Get), new { id = bus.Id }, bus);
+    }
+
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Bus bus)
+    {
+        if (id != bus.Id)
+        {
+            return BadRequest();
+        }
+        var existingBus = _service.Get(id);
+        if (existingBus is null)
+        {
+            return NotFound();
+        }
+
+        _service.Update(bus);
+        return NoContent();
+    }
+
+    // DELETE action
+    public IActionResult Delete(int id)
+    {
+        var bus = Get(id);
+        if (bus is null)
+        {
+            return NotFound();
+        }
+        _service.Delete(id);
+        return NoContent();
+    }
 }
--- Services/BusService.cs
+++ Services/BusService.cs
@@ -1,6 +1,7 @@
 using BusTicketSystem.Models;
 using BusTicketSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BusTicketSystem.Services;
 
@@ -8,12 +9,52 @@ public class BusService(TicketSystemContext context)
 {
     private readonly TicketSystemContext _context = context;
 
-    // CRUD Operations
-
     public IEnumerable<Bus> GetAll()
     {
         return _context.Buses
         .AsNoTracking();
     }
+    public Bus? Get(int id)
+    {
+        return _context.Buses
+        .Include(b => b.Id)
+        .Include(b => b.Capacity)
+        .Include(b => b.Model)
+        .Include(b => b.Routes)
+        .AsNoTracking()
+        .SingleOrDefault(b => b.Id == id);
 
+    }
+    public Bus Create(Bus newBus)
+    {
+        _context.Buses.Add(newBus);
+        _context.SaveChanges();
+        return newBus;
+    }
+    public void Update(Bus bus)
+    {
+        _context.Buses.Update(bus);
+        _context.SaveChanges();
+    }
+    public void Delete(int id)
+    {
+        var bus = _context.Buses.Find(id);
+        if (bus is not null)
+        {
+            _context.Buses.Remove(bus);
+            _context.SaveChanges();
+        }
+    }
+    public List<Models.Route>? AssignRoute(int id, Models.Route route)
+    {
+        var bus = Get(id);
+
+        return bus?.AssignRoute(route);
+    }
+    public List<Models.Route>? RemoveRoute(int id, Models.Route route)
+    {
+        var bus = Get(id);
+
+        return bus?.RemoveRoute(route);
+    }
 }
